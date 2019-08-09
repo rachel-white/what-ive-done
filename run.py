3efca221045ff2ce46d1362d5d5d0047e4e2012a
rachel-white <racheliwhite01@gmail.com>
2019-08-09 09:29:48 +0000

    added title_of_page variables

--- run.py
+++ run.py
@@ -2,28 +2,31 @@ import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'wid-dbr'
+# - re-learn about what this line means, then edit as nessacery. app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", title_of_page="Home - What I've Done")
         
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title_of_page="About - What I've Done")
     
 @app.route('/aboutsi')
 def aboutsi():
-    return render_template("aboutsi.html")
+    return render_template("aboutsi.html", title_of_page="About - What I've Done")
     
 @app.route('/today')
 def today():
-    return render_template("today.html")
+    return render_template("today.html", title_of_page="Today - What I've Done")
 
 @app.route('/history')
 def history():
-    return render_template("history.html")
+    return render_template("history.html", title_of_page="History - What I've Done")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
     debug=True)
+
