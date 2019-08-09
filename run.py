76fa39bc9e7477d7ae2882e3385f8c5dd9f2ad8a
rachel-white <racheliwhite01@gmail.com>
2019-08-09 08:21:50 +0000

    added basic routing for different pages

--- run.py
+++ run.py
@@ -1,4 +1,29 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template("home.html")
+        
+@app.route('/about')
+def about():
+    return render_template("about.html")
+    
+@app.route('/aboutsi')
+def aboutsi():
+    return render_template("aboutsi.html")
+    
+@app.route('/today')
+def today():
+    return render_template("today.html")
 
+@app.route('/history')
+def history():
+    return render_template("history.html")
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT')),
+    debug=True)
