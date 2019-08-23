0e02e7dec62107232e55b52f1b100701c0b539c1
rachel-white <racheliwhite01@gmail.com>
2019-08-23 10:27:36 +0000

    starting to connect js to html

---
+++ static/js/today.js
@@ -0,0 +1,6 @@
+//var date = new Date();
+//var timestamp = date.getTime();
+function getTime() {
+    document.getElementsByClassName("time").innerHTML = "Is the button connecting to this function";
+}
+
