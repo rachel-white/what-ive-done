b731eb8e30505eb64b5c492ccbc44e179dc16940
rachel-white <racheliwhite01@gmail.com>
2019-08-25 09:49:52 +0000

    when click p the time-to-add-db div text correctly displays timestamp

--- static/js/today.js
+++ static/js/today.js
@@ -1,6 +1,24 @@
 //var date = new Date();
 //var timestamp = date.getTime();
-function getTime() {
-    document.getElementsByClassName("time").innerHTML = "Is the button connecting to this function";
-}
+//function getTime() {
+  //  document.getElementsByClassName("time").innerHTML = "Is the button connecting to this function";
+//}
 
+
+//$(document).ready(function() { 
+  //          $(".time").click(function getTime(event) { 
+    //            $(".time").text(event.timeStamp); 
+      //      }); 
+        //}); 
+
+//$(document).ready(function() { 
+  //          $(".click-for-time").click(function(event) { 
+    //            $("span").text(event.timeStamp); 
+      //      }); 
+        //}); 
+
+$(document).ready(function() { 
+          $("p").click(function(event) { 
+             $(".time-add-to-db").text(event.timeStamp); 
+         }); 
+      }); 
