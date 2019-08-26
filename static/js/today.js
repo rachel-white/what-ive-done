72a8bc06eaccf272a7a81c70882f193d81e22ea1
rachel-white <racheliwhite01@gmail.com>
2019-08-26 07:29:06 +0000

    removed previous commented out code blocks

--- static/js/today.js
+++ static/js/today.js
@@ -1,22 +1,3 @@
-//var date = new Date();
-//var timestamp = date.getTime();
-//function getTime() {
-  //  document.getElementsByClassName("time").innerHTML = "Is the button connecting to this function";
-//}
-
-
-//$(document).ready(function() { 
-  //          $(".time").click(function getTime(event) { 
-    //            $(".time").text(event.timeStamp); 
-      //      }); 
-        //}); 
-
-//$(document).ready(function() { 
-  //          $(".click-for-time").click(function(event) { 
-    //            $("span").text(event.timeStamp); 
-      //      }); 
-        //}); 
-
 $(document).ready(function() { 
           $(".click-for-time").click(function(event) {
               var timestamp = new Date(event.timeStamp);
