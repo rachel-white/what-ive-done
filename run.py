900b95a80db1fa4a5f704d1e92356f635cfc5cd6
rachel-white <racheliwhite01@gmail.com>
2019-08-26 13:48:34 +0000

    added mongo database call in today route but sometign not working

--- run.py
+++ run.py
@@ -37,7 +37,7 @@ def add_achievement():
 @app.route('/today', methods=['GET', 'POST'])
 def today():
     session['userID'] = request.form["username"] #stores username as userID
-    return render_template("today.html", title_of_page="Today - What I've Done", userID=session['userID'])
+    return render_template("today.html", title_of_page="Today - What I've Done", userID=session['userID'], achievements=mongo.db.achievements.find())
 
 @app.route('/history')
 def history():
