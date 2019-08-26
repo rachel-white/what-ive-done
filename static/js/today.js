d2411f1180ade6df88e243b2b811e5092ad28897
rachel-white <racheliwhite01@gmail.com>
2019-08-26 11:18:20 +0000

    added console logs for time related vars, displaying incorrect date...

--- static/js/today.js
+++ static/js/today.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
               var date = timestamp.toDateString()
               var time = timestamp.toTimeString()
              $(".time-add-to-db").text(timestamp + "date: " + date + " " + "time: " + time);
+             console.log("timestamp:" + timestamp)
+             console.log(date + "date")
+             console.log(time + "time")
               document.getElementById('time').value=(time + "time var");
               document.getElementById('date').value=(date + "date var");
          }); 
