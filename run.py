1b635ab42c4ff5fb6bd9a05619f987fed71388e0
rachel-white <racheliwhite01@gmail.com>
2019-09-13 08:47:25 +0000

    working on recording todays date for loading only todays records

--- run.py
+++ run.py
@@ -8,6 +8,19 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'wid' #What Ive Done - Database
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 mongo = PyMongo(app)
+
+now = datetime.now() # current date and time
+year = now.strftime("%Y")
+print("year:", year)
+month = now.strftime("%m")
+print("month:", month)
+day = now.strftime("%d")
+print("day:", day)
+time = now.strftime("%H:%M:%S")
+print("time:", time)
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+print("date and time:",date_time)	
+
  
 @app.route('/', methods=['GET', 'POST'])
 def home():
