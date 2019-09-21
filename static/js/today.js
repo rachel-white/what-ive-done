d1a5e0cfa2032c5f83449ec735dd0bbc74ed3307
rachel-white <racheliwhite01@gmail.com>
2019-09-21 21:38:34 +0000

    removed unnesacery argument in function and consolelog

--- static/js/today.js
+++ static/js/today.js
@@ -1,12 +1,11 @@
-function generateTime(event) {
+function generateTime() {
               var timestamp = new Date();
               var date = timestamp.toDateString();
               var hourtime = timestamp.getHours();
               var minstime = timestamp.getMinutes();
               var secondstime = timestamp.getSeconds();
               var time = (hourtime + ":" + minstime +  ":" + secondstime); 
-              console.log("time: " + time)
               document.getElementById('time').value=(time);
               document.getElementById('date').value=(date);
          }; 
-         
+         5
